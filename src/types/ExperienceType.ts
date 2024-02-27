3c1bbeb436d1cf7e787135ba73992befa0676453
Cherubin <cherubinkubwayo@gmail.com>
2024-02-27 13:59:01 +0200

    add experience type

---
+++ src/types/ExperienceType.ts
@@ -0,0 +1,10 @@
+export type ExperienceType = {
+  role: string;
+  company: string;
+  duration: string;
+  location: string;
+  website: string;
+  description: string;
+  logo: string;
+  skills: string[];
+};
